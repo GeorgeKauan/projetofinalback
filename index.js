8582cc96cc7b646709e90f2ab501a2418926f805
Bruno Gomes <bruno_gurgel@yahoo.com.br>
2025-11-19 17:43:55 -0300

    Métodos atualizados para consultar do BD

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ app.listen(port, () => {
 })
 
 const produtos = [
-    { id: 1, nome: 'Smartphone Motorola G85', marca: 'Motorola', preco: 1800.00, quantidade: 50 }, 
-    { id: 2, nome: 'TV Lg 42 polegadas', marca: 'Motorola', preco: 1499.99, quantidade: 12 }
+  { id: 1, nome: 'Smartphone Motorola G85', marca: 'Motorola', preco: 1800.00, quantidade: 50 },
+  { id: 2, nome: 'TV Lg 42 polegadas', marca: 'Motorola', preco: 1499.99, quantidade: 12 }
 ]
 /** Ponto de entrada raíz da aplicação 
  * @param req objeto com os dados da requisição HTTP
@@ -36,41 +36,50 @@ const produtos = [
 app.get('/', (req, res) => { res.redirect('/produtos') })
 
 /** Devolve a lista de todos os produtos */
-app.get('/produtos', (req, res) => { 
-  connection.query('SELECT * FROM produto', (err, rows, fields) => {
-  if (err) throw err
-
-  console.log(rows)
-})
-
-  res.json(produtos) 
+app.get('/produtos', (req, res) => {
+  //connection.query realiza uma operação no BD (select, insert, update...)
+  const consulta = 'SELECT * FROM produto'
+  connection.query(consulta, (erro, produtos) => {
+    if (!erro) {
+      res.json(produtos)
+    } else {
+      console.log(`Erro: ${err.sqlMessage}`)
+      res.json([])
+    }
+  })
 })
 
 /** Devolve um produto específico pelo seu id */
 app.get('/produtos/:id', (req, res) => {
-    const id = +req.params.id
-    if (req.params.id && id >= 0) {
-        const prod = produtos.find(p => p.id === id)
-        if (prod) { 
-          res.json(prod) 
-        } else { 
-          res.json({}) 
-        }
-    } else {
-      res.json({})
-    }
+  const id = +req.params.id
+  if (req.params.id && id >= 0) {
+    const consulta = 'SELECT * FROM produto WHERE id=?'
+    connection.query(consulta, [id] ,(erro, produto) => {
+      if (!erro) {
+        res.json(produto)
+      } else {
+        console.log(`Erro: ${err.sqlMessage}`)
+        res.json([])
+      }
+    })
+  } else {
+    res.json([])
+  }
 })
 //Obter o preço de um produto dado o seu id
 app.get('/produtos/:id/preco', (req, res) => {
   const id = +req.params.id
-    if (req.params.id && id >= 0) {
-        const prod = produtos.find(p => p.id === id)
-        if (prod) { 
-          res.json(prod.preco) 
-        } else { 
-          res.json({}) 
-        }
-    } else {
-      res.json({})
-    }
+  if (req.params.id && id >= 0) {
+    const consulta = 'SELECT preco FROM produto WHERE id=?'
+    connection.query(consulta, [id] ,(erro, preco) => {
+      if (!erro) {
+        res.json(preco)
+      } else {
+        console.log(`Erro: ${err.sqlMessage}`)
+        res.json([])
+      }
+    })
+  } else {
+    res.json([])
+  }
 })
